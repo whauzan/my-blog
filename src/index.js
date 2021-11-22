df0b5093736961ed9982dd43a2be5bc5d224d78d
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2021-11-23 04:21:52 +0700

    add initial config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import '../src/Styles/global.scss'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
