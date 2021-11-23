92a67be29ce103ec9bb418e542600253d3234cb8
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2021-11-24 03:31:57 +0700

    refactor: delete dummy data

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,43 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { PostCard, Categories, PostWidget, Header } from '../Components';
-import image1 from '../img/deployment.png'
-
-const posts = [
-  {
-    title: 'React Testing',
-    excerpt: 'Learn React Testing',
-    image: {image1},
-    slug: 'react-testing',
-    author: {
-        name: 'Whauzan'
-    },
-    createdAt: 'August 08, 2021'
-  },
-  {
-    title: 'React Routing',
-    excerpt: 'Learn React Routing',
-    image: {image1},
-    slug: 'react-routing',
-    author: {
-        name: 'Whauzan'
-    },
-    createdAt: 'August 08, 2021'
-  }
-]
+import useGetPost from '../Hooks/useGetPost';
 
 function Home() {
+    const [data, setData] = useState([]);
+    const { dataGetPost, loadingGetPost, errorLoadingPost } = useGetPost();
+
+    useEffect(() => {
+        if (dataGetPost) {
+            setData(dataGetPost.post);
+        }
+    }, [dataGetPost])
+
+    // console.log(data);
+
     return (
         <>
         <Header />
-        <div className="container mx-auto px-10 mb-8">
+        <div className="container mx-auto px-10 mb-8 relative top-32">
             <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
                 <div className="lg:col-span-8 col-span-1">
-                    {posts.map((post) => (
-                        <PostCard post={post} key={post.title} />
+                    {data?.map((item) => (
+                        <PostCard post={item} key={item.title} />
                     ))}
                 </div>
                 <div className="lg:col-span-4 col-span-1">
-                    <div className="lg:sticky relative top-8">
+                    <div className="lg:sticky relative top-32">
                         <PostWidget />
                         <Categories />
                     </div>
