d1d6689866aee097e938b882559a702b19662226
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2021-11-24 03:33:03 +0700

    add dynamic routing for post detail

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home';
+import PostDetails from './Pages/PostDetails';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<Home />}/>
+          <Route path='/post/:slug' element={<PostDetails />}/>
         </Routes>
       </Router>
     </div>
