d5f888aedf9f63eebfb5005f658de8a508379807
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2021-11-23 22:28:23 +0700

    chore: add dot env

--- src/Apollo/client.js
+++ src/Apollo/client.js
@@ -3,7 +3,7 @@ import {ApolloClient, InMemoryCache} from '@apollo/client'
 const client = new ApolloClient({
   uri: 'https://huge-cow-26.hasura.app/v1/graphql',
   headers: {
-    'x-hasura-admin-secret': 'hFiE6Be1eY1DPnGSQEfoarH3E3ObI25y19qxz9tfrun9hQF06QWBIMGTkgfiMzOX'
+    'x-hasura-admin-secret': process.env.REACT_APP_API_KEY
   },
   cache: new InMemoryCache(),
 })
