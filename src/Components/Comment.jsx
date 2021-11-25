b971bab10043cbcd989515ff0910b49a850bb7eb
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2021-11-26 04:22:17 +0700

    feat: comment section done

--- src/Components/Comment.jsx
+++ src/Components/Comment.jsx
@@ -1,10 +1,27 @@
+import moment from 'moment'
 import React from 'react'
 
-function Comment() {
+function Comment({ post }) {
     return (
-        <div>
-            <h1>Comment</h1>
-        </div>
+        <>
+        {post?.length > 0 && (
+            <div className="bg-blue-light shadow-2xl rounded-lg p-8 pb-12 mb-8">
+                <h3 className="text-xl mb-8 font-semibold border-b pb-4 text-grey">{post?.length} Comment</h3>
+                {post.map((item, index) => (
+                    <div key={index} className="border-b border-grey-dark mb-4 pb-4">
+                        <p className="mb-4 text-grey">
+                            <span className="font-semibold text-grey">{item.name}</span>
+                            {' '}
+                            on
+                            {' '}
+                            {moment(item.created_at).format('MMM DD, YYYY')}
+                        </p>
+                        <p className="whitespace-pre-line text-grey-dark w-full">{item.comment}</p>
+                    </div>
+                ))}
+            </div>
+        )}
+        </>
     )
 }
 
