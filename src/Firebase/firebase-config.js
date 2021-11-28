86a94d512c23798046f1ec26e48de986f182eb24
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2021-11-29 01:46:55 +0700

    feat: config firebase

---
+++ src/Firebase/firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "whauzan-blog.firebaseapp.com",
+  projectId: "whauzan-blog",
+  storageBucket: "whauzan-blog.appspot.com",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
+
+export const app = initializeApp(firebaseConfig);
