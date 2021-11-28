434246c41364b115a9a3ef6447609a608ead5dad
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2021-11-29 02:22:15 +0700

    feat: pages callback easier

---
+++ src/Pages/index.js
@@ -0,0 +1,6 @@
+export { default as Category } from './Category'
+export { default as Dashboard } from './Dashboard'
+export { default as Home } from './Home'
+export { default as NewPost } from './NewPost'
+export { default as PostDetails } from './PostDetails'
+export { default as Posts } from './Posts'
