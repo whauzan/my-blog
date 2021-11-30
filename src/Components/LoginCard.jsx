63f10b370a58df8340273fea7e601a0f2b0aa23e
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2021-11-30 16:35:25 +0700

    feat: auth admin

--- src/Components/LoginCard.jsx
+++ src/Components/LoginCard.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import LockIcon from '@mui/icons-material/Lock';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router'
+import { useGetAuthorInfo } from '../Hooks';
 
 function LoginCard() {
+    const initialData = {
+        name: "",
+        password: "",
+    }
+    const navigate = useNavigate();
+
+    const { errorGetAuthorInfo, loadingGetAuthorInfo, dataGetAuthorInfo } = useGetAuthorInfo();
+    const [authorData, setAuthorData] = useState([]);
+    const [inputData, setInputData] = useState(initialData);
+
+    useEffect(() => {
+        if (dataGetAuthorInfo) {
+            setAuthorData(dataGetAuthorInfo.author[0]);
+        }
+    }, [dataGetAuthorInfo])
+
+    const handleInput = (e) => {
+        const nameTarget = e.target.name;
+        const value = e.target.value;
+        setInputData({
+            ...inputData,
+            [nameTarget]: value
+        })
+        console.log(inputData);
+    }
+    // console.log(authorData);
+
+    const handleSignIn = (e) => {
+        if (authorData.name === inputData.name && authorData.password === inputData.password) {
+            localStorage.setItem('name', authorData.name);
+            localStorage.setItem('password', authorData.password);
+            navigate("/admin/dashboard")
+        } else {
+            e.preventDefault();
+            alert("Username or Password Incorrect")
+            navigate("/admin")
+        }
+    }
+
     return (
     <>
     <div className="min-h-screen flex items-center justify-center px-4 sm:px-6 lg:px-8">
@@ -16,14 +57,15 @@ function LoginCard() {
                     <div className="rounded-md shadow-sm -space-y-px">
                         <div>
                             <label htmlFor="email-address" className="sr-only">
-                                Email address
+                                Username
                             </label>
                             <input
                             id="email-address"
-                            name="email"
-                            type="email"
+                            name="name"
+                            type="text"
                             autoComplete="email"
                             required
+                            onChange={handleInput}
                             className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                             placeholder="Username"
                             />
@@ -38,23 +80,23 @@ function LoginCard() {
                             type="password"
                             autoComplete="current-password"
                             required
+                            onChange={handleInput}
                             className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                             placeholder="Password"
                             />
                         </div>
                     </div>
                     <div>
-                        <Link to={`/admin/dashboard`}>
-                            <button
-                                type="submit"
-                                className="group relative w-full flex justify-center py-2 px-4 border border-green text-sm font-medium rounded-md text-green font-serif bg-blue hover:bg-blue-light focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green"
-                            >
-                                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                                <LockIcon className="h-5 w-5 text-green group-hover:text-green-light" aria-hidden="true" />
-                                </span>
-                                Sign in
-                            </button>
-                        </Link>
+                        <button
+                            type="submit"
+                            onClick={handleSignIn}
+                            className="group relative w-full flex justify-center py-2 px-4 border border-green text-sm font-medium rounded-md text-green font-serif bg-blue hover:bg-blue-light focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green"
+                        >
+                            <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                            <LockIcon className="h-5 w-5 text-green group-hover:text-green-light" aria-hidden="true" />
+                            </span>
+                            Sign in
+                        </button>
                     </div>
                 </form>
             </div>
