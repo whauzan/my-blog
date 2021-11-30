5096557727a0d7299e8c9b8c5ff3b9f3626c0079
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2021-12-01 03:56:23 +0700

    feat: config apollo for subscription

--- src/Apollo/client.js
+++ src/Apollo/client.js
@@ -1,11 +1,49 @@
-import {ApolloClient, InMemoryCache} from '@apollo/client'
+import {ApolloClient, HttpLink, InMemoryCache, split} from '@apollo/client'
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from '@apollo/client/utilities';
 
-const client = new ApolloClient({
+const wsLink = new WebSocketLink({
+  uri: 'wss://huge-cow-26.hasura.app/v1/graphql',
+  options: {
+    reconnect: true,
+    connectionParams: {
+      headers: {
+        'x-hasura-admin-secret': process.env.REACT_APP_API_KEY
+      }
+    }
+  }
+})
+
+const httpLink = new HttpLink({
   uri: 'https://huge-cow-26.hasura.app/v1/graphql',
   headers: {
     'x-hasura-admin-secret': process.env.REACT_APP_API_KEY
   },
+})
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+			definition.operation === "subscription"
+    )
+  },
+  wsLink,
+  httpLink
+)
+
+const client = new ApolloClient({
+  link: splitLink,
   cache: new InMemoryCache(),
 })
+
+// const client = new ApolloClient({
+//   uri: 'https://huge-cow-26.hasura.app/v1/graphql',
+//   headers: {
+//     'x-hasura-admin-secret': process.env.REACT_APP_API_KEY
+//   },
+//   cache: new InMemoryCache(),
+// })
 
 export default client;
